c91fe2dc5e88fa8e4a18c75bddef5c7b1426e6bf
Alexandre <pedro.javalabs.pt@gmail.com>
2023-09-17 00:19:55 +0100

    Introduce Person Service

--- src/main/java/com/javalabs/userfilemanager/service/PersonService.java
+++ src/main/java/com/javalabs/userfilemanager/service/PersonService.java
@@ -1,5 +1,9 @@
 package com.javalabs.userfilemanager.service;
 
+import java.util.Optional;
+
+import com.javalabs.userfilemanager.domain.Person;
+
 /**
  * Person Service Interface
  * Services related to Person
@@ -10,4 +14,8 @@ package com.javalabs.userfilemanager.service;
  */
 public interface PersonService {
 
+	public void add(Person person);
+	public Optional<Person> retrieve(Long id);
+	public void update(Person person);
+	public void delete(Long id);
 }
--- src/main/java/com/javalabs/userfilemanager/service/impl/PersonServiceImpl.java
+++ src/main/java/com/javalabs/userfilemanager/service/impl/PersonServiceImpl.java
@@ -1,7 +1,13 @@
 package com.javalabs.userfilemanager.service.impl;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.javalabs.userfilemanager.domain.File;
+import com.javalabs.userfilemanager.domain.Person;
+import com.javalabs.userfilemanager.repository.PersonRepository;
 import com.javalabs.userfilemanager.service.PersonService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -17,5 +23,31 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 public class PersonServiceImpl extends BaseServiceImpl implements PersonService  {
+
+	@Autowired
+	PersonRepository personRepository;
+	
+	@Override
+	public void add(Person person) {
+		personRepository.save(person);		
+	}
+
+	@Override
+	public Optional<Person> retrieve(Long id) {
+		return personRepository.findById(id);
+	}
+
+	@Override
+	public void update(Person person) {
+		personRepository.save(person);		
+	}
+
+	@Override
+	public void delete(Long id) {
+		Optional<Person> foundPerson = personRepository.findById(id);
+		if (foundPerson.isPresent()) {
+			personRepository.delete(foundPerson.get());
+		}	
+	}
 
 }
