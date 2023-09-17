1451ee8c246fe11e2aafbfd89fec9c4d61c23a2f
Alexandre <pedro.javalabs.pt@gmail.com>
2023-09-17 01:45:50 +0100

    Introduce Role and Privilege

---
+++ src/main/java/com/javalabs/userfilemanager/domain/Privilege.java
@@ -0,0 +1,49 @@
+package com.javalabs.userfilemanager.domain;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * Privilege Entity  
+ *
+ * A Privilege is a Collection of Roles
+ * 
+ * @author Alexandre Tavares - JavaLabs
+ * @since Feb 2021
+ * 
+ */
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@ToString
+@Entity
+public class Privilege {
+
+	private static final long serialVersionUID = 2950405302158037473L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    @ManyToMany(mappedBy = "privileges")
+    private Collection<Role> roles;
+
+    public Privilege() {
+        super();
+    }
+
+    public Privilege(final String name) {
+        super();
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/javalabs/userfilemanager/domain/Role.java
@@ -0,0 +1,57 @@
+package com.javalabs.userfilemanager.domain;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * Role Entity  
+ *
+ * An Authorization Role
+ * 
+ * @author Alexandre Tavares - JavaLabs
+ * @since Feb 2021
+ * 
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@ToString
+@Entity
+public class Role {
+
+	private static final long serialVersionUID = 5570852700686895664L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+	private String name;
+    @ManyToMany(mappedBy = "roles")
+    private Collection<Person> people;
+    @ManyToMany
+    @JoinTable(
+    	name = "roles_privileges", 
+    	joinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"), 
+    	inverseJoinColumns = @JoinColumn(name = "privilege_id", referencedColumnName = "id")
+    )
+    private Collection<Privilege> privileges;
+
+    public Role(final String name) {
+        super();
+        this.name = name;
+    }
+    
+}
