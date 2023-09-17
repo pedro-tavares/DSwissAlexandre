f13f6ce3284476092f51ca72e47de334ddfb81d9
Alexandre <pedro.javalabs.pt@gmail.com>
2023-09-17 01:18:12 +0100

    Introduce JWT Token Util

---
+++ src/main/java/com/javalabs/userfilemanager/security/SecurityConstants.java
@@ -0,0 +1,18 @@
+package com.javalabs.userfilemanager.security;
+
+/**
+ * Security Constants   
+ *
+ * @author Alexandre Tavares - JavaLabs
+ * @since Feb 2021
+ * 
+ */
+public class SecurityConstants {
+	
+	public static final String SECRET = "SECRET_KEY";
+	public static final long EXPIRATION_TIME = 900_000; // 15 mins
+	public static final String TOKEN_PREFIX = "Bearer ";
+	public static final String HEADER_STRING = "Authorization";
+	public static final String REGISTER_URL = "/person/register";
+	
+}
